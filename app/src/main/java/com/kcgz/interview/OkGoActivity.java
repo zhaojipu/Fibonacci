51fbb9adb195590bd500ace4ec29ee0b90664e63
zhaojipu <zhaojipu@99114.com>
2017-03-29 16:40:33 +0800

    圆形带进度的进度条

--- app/src/main/java/com/kcgz/interview/OkGoActivity.java
+++ app/src/main/java/com/kcgz/interview/OkGoActivity.java
@@ -1,15 +1,23 @@
 package com.kcgz.interview;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 
-public class OkGoActivity extends AppCompatActivity {
+import java.io.File;
 
+public class OkGoActivity extends AppCompatActivity {
+    File file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ok_go);
+        boolean storageEmulated = Environment.isExternalStorageEmulated();
+        if (storageEmulated){
+            file= Environment.getExternalStorageDirectory();
 
+        }else
+            file=Environment.getRootDirectory();
 
 
     }
